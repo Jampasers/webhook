bfc8c162b5314bb8adb6ed59c1e5e719809282b0
Jampasers <m.zidanea.j@gmail.com>
2026-02-16 11:21:26 +0700

    awikwok

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,16 +2,16 @@ export const config = {
     // Payment Gateway Credentials (sama dengan config.js di root)
     paymentGateway: {
         tokopay: {
-            merchantId: process.env.TOKOPAY_MERCHANT_ID || 'M260119CPIOA131',
-            secret: process.env.TOKOPAY_SECRET || 'aa724c719c5e799625bb65c5cb2f0f1748173b0d7769e9710b5bcade506b1631',
+            merchantId: process.env.TOKOPAY_MERCHANT_ID || '',
+            secret: process.env.TOKOPAY_SECRET || '',
         },
         pakasir: {
-            projectSlug: process.env.PAKASIR_PROJECT_SLUG || 'dankastur',
-            apiKey: process.env.PAKASIR_API_KEY || 'F5CQeayUurAmZYXcmoiHYlcVhOzXNUHD',
+            projectSlug: process.env.PAKASIR_PROJECT_SLUG || '',
+            apiKey: process.env.PAKASIR_API_KEY || '',
         },
         qrispw: {
-            apiKey: process.env.QRISPW_API_KEY || '3f22e2f28c003d59d6bdf2c5fc42ad6d4066cdc03aeed86fd3238cb6124bf5a5',
-            apiSecret: process.env.QRISPW_API_SECRET || '07881a03dad6d9506cc566bda128f75b81205fd446e6c69dada596631e1b88de',
+            apiKey: process.env.QRISPW_API_KEY || '',
+            apiSecret: process.env.QRISPW_API_SECRET || '',
         },
         duitku: {
             merchantCode: process.env.DUITKU_MERCHANT_CODE || 'YOUR_MERCHANT_CODE',
